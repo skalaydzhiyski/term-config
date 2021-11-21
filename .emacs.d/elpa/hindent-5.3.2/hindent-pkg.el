4fbce75ebe15296c8f55e8333529fdaa345ad8af
darchitect <darchitect@armitage>
2021-11-21 19:11:16 +0000

    latest change + emacs conf

---
+++ .emacs.d/elpa/hindent-5.3.2/hindent-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from hindent-5.3.2/hindent.el  -*- no-byte-compile: t -*-
+(define-package "hindent" "5.3.2" "Indent haskell code using the \"hindent\" program" '((cl-lib "0.5")) :commit "e146d672ea4c00fe17018e7e76e49d59570eeb2b" :authors '(("Chris Done" . "chrisdone@gmail.com")) :maintainer '("Chris Done" . "chrisdone@gmail.com") :url "https://github.com/chrisdone/hindent")
---
+++ .emacs.d/elpa/modus-vivendi-theme-0.13.2/modus-vivendi-theme-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from modus-vivendi-theme.el  -*- no-byte-compile: t -*-
+(define-package "modus-vivendi-theme" "0.13.2" "Accessible dark theme (WCAG AAA) [DEPRECATED]" '((emacs "26.1") (modus-themes "1.2.4")) :keywords '("faces" "theme" "accessibility") :authors '(("Protesilaos Stavrou" . "info@protesilaos.com")) :maintainer '("Protesilaos Stavrou" . "info@protesilaos.com") :url "https://gitlab.com/protesilaos/modus-themes")
