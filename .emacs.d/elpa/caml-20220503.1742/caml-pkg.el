303f92e2d5942c4c718871b91556d289a29e91d4
skalaydzhiyski <spas.kalaydzhiyski@gmail.com>
2023-05-23 11:39:02 +0100

    copy emacs.d

---
+++ .emacs.d/elpa/caml-20220503.1742/caml-pkg.el
@@ -0,0 +1,14 @@
+(define-package "caml" "20220503.1742" "Caml mode for GNU Emacs"
+  '((emacs "24.3"))
+  :commit "d3d2f5dbeccd0f415cbb28fab2015dc678e80bbd" :authors
+  '(("Jacques Garrigue" . "garrigue@kurims.kyoto-u.ac.jp")
+    ("Ian T Zimmerman" . "itz@rahul.net")
+    ("Damien Doligez" . "damien.doligez@inria.fr"))
+  :maintainer
+  '("Christophe Troestler" . "Christophe.Troestler@umons.ac.be")
+  :keywords
+  '("ocaml")
+  :url "https://github.com/ocaml/caml-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .emacs.d/elpa/dune-20220805.1652/dune-pkg.el
@@ -0,0 +1,4 @@
+(define-package "dune" "20220805.1652" "Integration with the dune build system" 'nil :commit "3df932f7f91ea68c3fee789f133b4aa8f9bea807" :url "https://github.com/ocaml/dune")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
