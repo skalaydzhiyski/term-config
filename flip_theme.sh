cfeb675977c4b3e97ecdba09f697aac585478a1e
darchitect <spas.kalaydzhiyski@gmail.com>
2024-02-16 18:34:47 +0000

    flip utop too

--- flip_theme.sh
+++ flip_theme.sh
@@ -4,9 +4,11 @@ if [ $theme -eq 0 ]
 then
    echo 'dark mode!'
    cp .emacs.dark .emacs
+   cp .utoprc.dark .utoprc
    cp .config/alacritty/alacritty.yml.dark .config/alacritty/alacritty.yml
 else
    echo 'light mode!'
    cp .emacs.light .emacs
+   cp .utoprc.light .utoprc
    cp .config/alacritty/alacritty.yml.light .config/alacritty/alacritty.yml
 fi
